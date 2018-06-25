4c7b437acc6346308d0ba12686f6db0900270ff1
Soo Kim <soo@haru.camp>
2018-06-25 18:18:05 +0900

    Edit coding convention

--- harupy/text.py
+++ harupy/text.py
@@ -21,239 +21,238 @@ from unicodedata import normalize
 
 
 class String(str):
-	"""
-	기본 내장객체 str를 확장하여 한글 처리 로직이 추가된 문자열 오브젝트
-	"""
-	NUMBER = False
+    """
+    기본 내장객체 str를 확장하여 한글 처리 로직이 추가된 문자열 오브젝트
+    """
+    NUMBER = False
 
-	def __new__(cls, object):
-		return str.__new__(cls, object)
+    def __new__(cls, object):
+        return str.__new__(cls, object)
 
-	def __add__(self, other):
-		return String(super(String, self).__add__(other))
+    def __add__(self, other):
+        return String(super(String, self).__add__(other))
 
-	def __radd__(self, other):
-		return String(other.__add__(self))
+    def __radd__(self, other):
+        return String(other.__add__(self))
 
-	def __mul__(self, other):
-		return String(super(String, self).__mul__(other))
+    def __mul__(self, other):
+        return String(super(String, self).__mul__(other))
 
-	def __rmul__(self, other):
-		return String(super(String, self).__rmul__(other))
+    def __rmul__(self, other):
+        return String(super(String, self).__rmul__(other))
 
-	def __getitem__(self, item):
-		return String(super(String, self).__getitem__(item))
+    def __getitem__(self, item):
+        return String(super(String, self).__getitem__(item))
 
-	def __iter__(self):
-		for s in self.__str__():
-			yield String(s)
+    def __iter__(self):
+        for s in self.__str__():
+            yield String(s)
 
-	def __getattribute__(self, item):
-		if 44031 < ord(item[0]) < 55204:
-			if String(item).hangul_rate() == 100:
-				return self.josa(item)
-		rtn = super(String, self).__getattribute__(item)
-		if rtn.__class__.__name__ in ('method', 'builtin_function_or_method'):
-			return self.string_object_decorator(rtn)
-		else:
-			return rtn
+    def __getattribute__(self, item):
+        if 44031 < ord(item[0]) < 55204:
+            if String(item).hangul_rate() == 100:
+                return self.josa(item)
+        rtn = super(String, self).__getattribute__(item)
+        if rtn.__class__.__name__ in ('method', 'builtin_function_or_method'):
+            return self.string_object_decorator(rtn)
+        else:
+            return rtn
 
-	@staticmethod
-	def string_object_decorator(func):
-		"""
-		String 객체의 각 메소드에서 리턴 데이터를 다시 String 객체로 변환하는 데코레이터
-		"""
-		@wraps(func)
-		def wrapper(*args, **kwargs):
-			result = func(*args, **kwargs)
-			if type(result) is str:
-				return String(result)
-			elif type(result) is tuple:
-				return tuple([String(s) for s in result])
-			elif type(result) is list:
-				return [String(s) for s in result]
-			else:
-				return result
+    @staticmethod
+    def string_object_decorator(func):
+        """
+        String 객체의 각 메소드에서 리턴 데이터를 다시 String 객체로 변환하는 데코레이터
+        """
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if type(result) is str:
+                return String(result)
+            elif type(result) is tuple:
+                return tuple([String(s) for s in result])
+            elif type(result) is list:
+                return [String(s) for s in result]
+            else:
+                return result
 
-		return wrapper
+        return wrapper
 
-	def initialize_number(self):
-		self.NUMBER = ('', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구')
-		self.NUMBER_10 = ('', '십', '백', '천')
-		self.NUMBER_10K = ('', '만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극')
+    def initialize_number(self):
+        self.NUMBER = ('', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구')
+        self.NUMBER_10 = ('', '십', '백', '천')
+        self.NUMBER_10K = ('', '만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극')
 
-	def to_hangul(self, read_one=False):
-		if not super(String, self).isdecimal():
-			raise ValueError('Value must be integer-like string.')
+    def to_hangul(self, read_one=False):
+        if not super(String, self).isdecimal():
+            raise ValueError('Value must be integer-like string.')
 
-		if not self.NUMBER:
-			self.initialize_number()
+        if not self.NUMBER:
+            self.initialize_number()
 
-		number = self.__str__()
+        number = self.__str__()
 
-		if number == '0':
-			return '영'
-		if number == '1':
-			return self.NUMBER[1]
+        if number == '0':
+            return '영'
+        if number == '1':
+            return self.NUMBER[1]
 
-		def _read_under_10k(under_10k):
-			reading_10k = ''
-			for i in range(0, len(under_10k)):
-				reading_number = self.NUMBER[int(under_10k[-i-1])] if under_10k[-i-1] != '1' or i == 0 or read_one else ''
-				reading_10k = ((reading_number + self.NUMBER_10[i]) if under_10k[-i-1] != '0' else '') + reading_10k
-			return reading_10k
+        def _read_under_10k(under_10k):
+            reading_10k = ''
+            for i in range(0, len(under_10k)):
+                reading_number = self.NUMBER[int(under_10k[-i-1])] if under_10k[-i-1] != '1' or i == 0 or read_one else ''
+                reading_10k = ((reading_number + self.NUMBER_10[i]) if under_10k[-i-1] != '0' else '') + reading_10k
+            return reading_10k
 
-		# 4문자씩 나눠 읽기
-		i = 0
-		reading = ''
-		while number:
-			if len(number) > 4:
-				split = number[-4:]
-				number = number[:-4]
-			else:
-				split = number
-				number = ''
-			prefix = _read_under_10k(split)
-			reading = prefix + (self.NUMBER_10K[i] if prefix else '') + reading
-			i += 1
-		return reading
+        # 4문자씩 나눠 읽기
+        i = 0
+        reading = ''
+        while number:
+            if len(number) > 4:
+                split = number[-4:]
+                number = number[:-4]
+            else:
+                split = number
+                number = ''
+            prefix = _read_under_10k(split)
+            reading = prefix + (self.NUMBER_10K[i] if prefix else '') + reading
+            i += 1
+        return reading
 
-	def to_number(self):
-		if not self.NUMBER:
-			self.initialize_number()
-		number_str = self.__str__()
+    def to_number(self):
+        if not self.NUMBER:
+            self.initialize_number()
+        number_str = self.__str__()
 
-		if number_str in ('영', '공'):
-			return 0
+        if number_str in ('영', '공'):
+            return 0
 
-		number_list = []
-		for n in number_str:
-			if n in self.NUMBER:
-				number_list.append(self.NUMBER.index(n))
-			elif n in self.NUMBER_10:
-				unit_10 = 10 ** self.NUMBER_10.index(n)
-				if len(number_list) and number_list[-1] < 10:
-					number_list[-1] *= unit_10
-				else:
-					number_list.append(unit_10)
-			elif n in self.NUMBER_10K:
-				under_10k = 0
-				while len(number_list) and number_list[-1] < 10000:
-					under_10k += number_list.pop()
-				number_list.append((under_10k or 1) * (10000 ** self.NUMBER_10K.index(n)))
-			else:
-				raise ValueError('%s is invalid. It must be numeric Hangul words.' % n)
+        number_list = []
+        for n in number_str:
+            if n in self.NUMBER:
+                number_list.append(self.NUMBER.index(n))
+            elif n in self.NUMBER_10:
+                unit_10 = 10 ** self.NUMBER_10.index(n)
+                if len(number_list) and number_list[-1] < 10:
+                    number_list[-1] *= unit_10
+                else:
+                    number_list.append(unit_10)
+            elif n in self.NUMBER_10K:
+                under_10k = 0
+                while len(number_list) and number_list[-1] < 10000:
+                    under_10k += number_list.pop()
+                number_list.append((under_10k or 1) * (10000 ** self.NUMBER_10K.index(n)))
+            else:
+                raise ValueError('%s is invalid. It must be numeric Hangul words.' % n)
 
-		return sum(number_list)
+        return sum(number_list)
 
-	def isnumeric(self):
-		"""
-		한글로 읽은 숫자까지 결과에 반영
-		:return: bool
-		"""
-		result = super(String, self).isnumeric()
-		if not result:
-			if not self.NUMBER:
-				self.initialize_number()
-			number_strings = self.NUMBER + self.NUMBER_10 + self.NUMBER_10K + ('영', '공')
-			if sum([0 if s in number_strings else 1 for s in self]) == 0:
-				return True
-		return result
+    def isnumeric(self):
+        """
+        한글로 읽은 숫자까지 결과에 반영
+        :return: bool
+        """
+        result = super(String, self).isnumeric()
+        if not result:
+            if not self.NUMBER:
+                self.initialize_number()
+            number_strings = self.NUMBER + self.NUMBER_10 + self.NUMBER_10K + ('영', '공')
+            if sum([0 if s in number_strings else 1 for s in self]) == 0:
+                return True
+        return result
 
-	def hangul_rate(self):
-		"""
-		전체 문자열에서 한글인 문자열의 백분율(%)을 반환
-		:return: int
-		"""
-		total = len(self)
-		hangul_count = 0
-		for i in range(total):
-			code_num = ord(self[i])
-			if 44031 < code_num < 55204 or 12592 < code_num < 12644:
-				hangul_count += 100
-		return int(hangul_count / total)
+    def hangul_rate(self):
+        """
+        전체 문자열에서 한글인 문자열의 백분율(%)을 반환
+        :return: int
+        """
+        total = len(self)
+        hangul_count = 0
+        for i in range(total):
+            code_num = ord(self[i])
+            if 44031 < code_num < 55204 or 12592 < code_num < 12644:
+                hangul_count += 100
+        return int(hangul_count / total)
 
-	def extract_readable(self, only_hangul=False):
-		readable = ''
-		for s in self:
-			code_num = ord(s)
-			if 44031 < code_num < 55204:
-				readable += s
-			elif only_hangul is False and (12592 < code_num < 12623 or 12622 < code_num < 12644 \
-					or 96 < code_num < 123 or 64 < code_num < 91 or 47 < code_num < 58):
-				readable += s
-		return readable
+    def extract_readable(self, only_hangul=False):
+        readable = ''
+        for s in self:
+            code_num = ord(s)
+            if 44031 < code_num < 55204:
+                readable += s
+            elif only_hangul is False and (12592 < code_num < 12623 or 12622 < code_num < 12644 or
+                                           96 < code_num < 123 or 64 < code_num < 91 or 47 < code_num < 58):
+                readable += s
+        return readable
 
-	def get_last_bachim(self):
-		"""
-		마지막 글자 또는 마지막 글자의 읽는 방법을 기준으로 받침을 반환함
-		받침이 없는 경우 빈 문자열('') 반환
-		단, 알파벳의 경우
-		받침을 알 수 없는 경우 None을 반환
-		"""
-		readable = self.extract_readable()
-		if not readable:
-			return None
-		code_num = ord(readable[-1])
+    def get_last_bachim(self):
+        """
+        마지막 글자 또는 마지막 글자의 읽는 방법을 기준으로 받침을 반환함
+        받침이 없는 경우 빈 문자열('') 반환
+        단, 알파벳의 경우
+        받침을 알 수 없는 경우 None을 반환
+        """
+        readable = self.extract_readable()
+        if not readable:
+            return None
+        code_num = ord(readable[-1])
 
-		if 47 < code_num < 58:
-			# 숫자
-			return (
-				'ㅇ', 'ㄹ', '', 'ㅁ', '', '', 'ㄱ', 'ㄹ', 'ㄹ', ''
-			)[code_num - 48]
-		elif 64 < code_num < 91:
-			# 알파벳 대문자
-			return (
-				'', '', '', '', '', '', '', '', '', '', '', 'ㄹ', 'ㅁ', 'ㄴ', '', '', '', 'ㄹ',
-				'', '', '', '', '', '', '', ''
-			)[code_num - 65]
-		elif 96 < code_num < 123:
-			# 알파벳 소문자
-			return (
-				'', 'ㅂ', 'ㄱ', '', '', '', 'ㄱ', '', '', '', 'ㄱ', 'ㄹ', 'ㅁ', 'ㄴ', '', 'ㅂ', '', '',
-				'', '', '', '', '', '', '', ''
-			)[code_num - 97]
-		elif 44031 < code_num < 55204:
-			# 한글
-			return (
-				'ㄹㅅ', 'ㄹㅌ', 'ㄹㅍ', 'ㄹㅎ', 'ㅁ', 'ㅂ', 'ㅂㅅ', 'ㅅ', 'ㅅㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ',
-				'ㅍ', 'ㅎ', '', 'ㄱ', 'ㄱㄱ', 'ㄱㅅ', 'ㄴ', 'ㄴㅈ', 'ㄴㅎ', 'ㄷ', 'ㄹ', 'ㄹㄱ', 'ㄹㅁ', 'ㄹㅂ'
-			)[code_num % 28]
-		elif 12592 < code_num < 12623:
-			# 한글 자음
-			return (
-				'ㄱ', 'ㄱ', 'ㅅ', 'ㄴ', 'ㅈ', 'ㅎ', 'ㄷ', 'ㄷ', 'ㄹ', 'ㄱ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅌ', 'ㅍ',
-				'ㅎ', 'ㅁ', 'ㅂ', 'ㅂ', 'ㅅ', 'ㅅ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
-			)[code_num - 12593]
-		elif 12622 < code_num < 12644:
-			# 한글 모음
-			return ''
+        if 47 < code_num < 58:
+            # 숫자
+            return (
+                'ㅇ', 'ㄹ', '', 'ㅁ', '', '', 'ㄱ', 'ㄹ', 'ㄹ', ''
+            )[code_num - 48]
+        elif 64 < code_num < 91:
+            # 알파벳 대문자
+            return (
+                '', '', '', '', '', '', '', '', '', '', '', 'ㄹ', 'ㅁ', 'ㄴ', '', '', '', 'ㄹ',
+                '', '', '', '', '', '', '', ''
+            )[code_num - 65]
+        elif 96 < code_num < 123:
+            # 알파벳 소문자
+            return (
+                '', 'ㅂ', 'ㄱ', '', '', '', 'ㄱ', '', '', '', 'ㄱ', 'ㄹ', 'ㅁ', 'ㄴ', '', 'ㅂ', '', '',
+                '', '', '', '', '', '', '', ''
+            )[code_num - 97]
+        elif 44031 < code_num < 55204:
+            # 한글
+            return (
+                'ㄹㅅ', 'ㄹㅌ', 'ㄹㅍ', 'ㄹㅎ', 'ㅁ', 'ㅂ', 'ㅂㅅ', 'ㅅ', 'ㅅㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ',
+                'ㅍ', 'ㅎ', '', 'ㄱ', 'ㄱㄱ', 'ㄱㅅ', 'ㄴ', 'ㄴㅈ', 'ㄴㅎ', 'ㄷ', 'ㄹ', 'ㄹㄱ', 'ㄹㅁ', 'ㄹㅂ'
+            )[code_num % 28]
+        elif 12592 < code_num < 12623:
+            # 한글 자음
+            return (
+                'ㄱ', 'ㄱ', 'ㅅ', 'ㄴ', 'ㅈ', 'ㅎ', 'ㄷ', 'ㄷ', 'ㄹ', 'ㄱ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅌ', 'ㅍ',
+                'ㅎ', 'ㅁ', 'ㅂ', 'ㅂ', 'ㅅ', 'ㅅ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+            )[code_num - 12593]
+        elif 12622 < code_num < 12644:
+            # 한글 모음
+            return ''
 
-	def josa(self, josa_str):
-		"""
-		앞글자의 받침에 따라 변화하는 조사 붙이기
-		:param josa_str: 붙일 조사
-		:return: 적절한 조사를 붙인 문자열
-		"""
-		if type(josa_str) not in (str, String):
-			raise ValueError('Josa must be string type.')
-		if self == '':
-			raise ValueError('String value must not be empty.')
+    def josa(self, josa_str):
+        """
+        앞글자의 받침에 따라 변화하는 조사 붙이기
+        :param josa_str: 붙일 조사
+        :return: 적절한 조사를 붙인 문자열
+        """
+        if type(josa_str) not in (str, String):
+            raise ValueError('Josa must be string type.')
+        if self == '':
+            raise ValueError('String value must not be empty.')
 
-		josa_tuple = ('으로', '로', '이', '가', '은', '는', '을', '를', '과', '와', '아', '야')
+        josa_tuple = ('으로', '로', '이', '가', '은', '는', '을', '를', '과', '와', '아', '야')
 
-		if josa_str not in josa_tuple:
-			# '이'로 시작하는 모든 조사 및 술어
-			# '이랑', '랑', '이여', '여', '이다', '다', '이고', '고', '이며', '며', '이라고', '라고', '이라며', '라며', '이라면', '라면',
-			# '이라는', '라는', '이라서', '라서', '이야말로', '야말로' 등등...
-			return self + ('', '이')[1 if self.get_last_bachim() else 0] + josa_str.lstrip('이')
+        if josa_str not in josa_tuple:
+            # '이'로 시작하는 모든 조사 및 술어
+            # '이랑', '랑', '이여', '여', '이다', '다', '이고', '고', '이며', '며',
+            # '이라고', '라고', '이라며', '라며', '이라면', '라면',
+            # '이라는', '라는', '이라서', '라서', '이야말로', '야말로' 등등...
+            return self + ('', '이')[1 if self.get_last_bachim() else 0] + josa_str.lstrip('이')
 
-		index = (josa_tuple.index(josa_str) // 2) * 2
-		bachim = self.get_last_bachim()
+        index = (josa_tuple.index(josa_str) // 2) * 2
+        bachim = self.get_last_bachim()
 
-		return self + josa_tuple[index + (0 if bachim and (bachim != 'ㄹ' or index) else 1)]
+        return self + josa_tuple[index + (0 if bachim and (bachim != 'ㄹ' or index) else 1)]
 
-	def normalize(self, form='NFKD'):
-		return normalize(form, self)
-
-
+    def normalize(self, form='NFKD'):
+        return normalize(form, self)
--- tests/test_text.py
+++ tests/test_text.py
@@ -9,61 +9,61 @@ from harupy.text import String
 
 
 class TextTest(unittest.TestCase):
-	"""
-	조사 테스트
-	"""
-	def test_josa(self):
-		self.assertEqual(String('게').josa('나'), '게나')
-		self.assertEqual(String('게').josa('이나'), '게나')
-		self.assertEqual(String('고동').josa('나'), '고동이나')
-		self.assertEqual(String('고동').josa('이나'), '고동이나')
-		self.assertEqual(String('학교').josa('을'), '학교를')
-		self.assertEqual(String('학교').josa('를'), '학교를')
-		self.assertEqual(String('학교').josa('를'), '학교를')
-		self.assertEqual(String('Ace').josa('가'), 'Ace가')
-		self.assertEqual(String('makers').josa('가'), 'makers가')
-		self.assertEqual(String('pop').josa('가'), 'pop이')
-		self.assertEqual(String('funky').josa('가'), 'funky가')
-		self.assertEqual(String('music').josa('가'), 'music이')
-		self.assertEqual(String('JFK').josa('가'), 'JFK가')
-		self.assertEqual(String('2PM').josa('가'), '2PM이')
-		self.assertEqual(String('1234').josa('가'), '1234가')
-		self.assertEqual(String('123').josa('가'), '123이')
-		self.assertEqual(String('밥').josa('를'), '밥을')
-		self.assertEqual(String('사과').josa('을'), '사과를')
-		self.assertEqual(String('명함').josa('을'), '명함을')
-		self.assertEqual(String('븅신').josa('다'), '븅신이다')
-		self.assertEqual(String('똥개').josa('이다'), '똥개다')
-		self.assertEqual(String('작심삼일').josa('으로'), '작심삼일로')
-		self.assertEqual(String('작전변경').josa('로'), '작전변경으로')
-		self.assertEqual(String('ㄹ').josa('로'), 'ㄹ로')
-		self.assertEqual(String('ㅁ').josa('로'), 'ㅁ으로')
-		self.assertEqual(String('311').josa('이라고'), '311이라고')
-		self.assertEqual(String('312').josa('이라고'), '312라고')
-		self.assertEqual(String('317').josa('라고'), '317이라고')
-		self.assertEqual(String('318').josa('으로'), '318로')
-		self.assertEqual(String('313').josa('으로'), '313으로')
-		self.assertEqual(String('31').josa('으로'), '31로')
+    """
+    조사 테스트
+    """
+    def test_josa(self):
+        self.assertEqual(String('게').josa('나'), '게나')
+        self.assertEqual(String('게').josa('이나'), '게나')
+        self.assertEqual(String('고동').josa('나'), '고동이나')
+        self.assertEqual(String('고동').josa('이나'), '고동이나')
+        self.assertEqual(String('학교').josa('을'), '학교를')
+        self.assertEqual(String('학교').josa('를'), '학교를')
+        self.assertEqual(String('학교').josa('를'), '학교를')
+        self.assertEqual(String('Ace').josa('가'), 'Ace가')
+        self.assertEqual(String('makers').josa('가'), 'makers가')
+        self.assertEqual(String('pop').josa('가'), 'pop이')
+        self.assertEqual(String('funky').josa('가'), 'funky가')
+        self.assertEqual(String('music').josa('가'), 'music이')
+        self.assertEqual(String('JFK').josa('가'), 'JFK가')
+        self.assertEqual(String('2PM').josa('가'), '2PM이')
+        self.assertEqual(String('1234').josa('가'), '1234가')
+        self.assertEqual(String('123').josa('가'), '123이')
+        self.assertEqual(String('밥').josa('를'), '밥을')
+        self.assertEqual(String('사과').josa('을'), '사과를')
+        self.assertEqual(String('명함').josa('을'), '명함을')
+        self.assertEqual(String('븅신').josa('다'), '븅신이다')
+        self.assertEqual(String('똥개').josa('이다'), '똥개다')
+        self.assertEqual(String('작심삼일').josa('으로'), '작심삼일로')
+        self.assertEqual(String('작전변경').josa('로'), '작전변경으로')
+        self.assertEqual(String('ㄹ').josa('로'), 'ㄹ로')
+        self.assertEqual(String('ㅁ').josa('로'), 'ㅁ으로')
+        self.assertEqual(String('311').josa('이라고'), '311이라고')
+        self.assertEqual(String('312').josa('이라고'), '312라고')
+        self.assertEqual(String('317').josa('라고'), '317이라고')
+        self.assertEqual(String('318').josa('으로'), '318로')
+        self.assertEqual(String('313').josa('으로'), '313으로')
+        self.assertEqual(String('31').josa('으로'), '31로')
 
-	def test_to_hangul(self):
-		self.assertEqual(String(11231).to_hangul(), '일만천이백삼십일')
-		self.assertEqual(String(1110000).to_hangul(), '백십일만')
-		self.assertEqual(String(1110000).to_hangul(True), '일백일십일만')
+    def test_to_hangul(self):
+        self.assertEqual(String(11231).to_hangul(), '일만천이백삼십일')
+        self.assertEqual(String(1110000).to_hangul(), '백십일만')
+        self.assertEqual(String(1110000).to_hangul(True), '일백일십일만')
 
-	def test_to_number(self):
-		self.assertEqual(String('천이백십일억천백만').to_number(), 121111000000)
-		self.assertEqual(String('삼천이백십일억천백만오십칠').to_number(), 321111000057)
-		self.assertEqual(String('육경사천칠조오백오십만삼백일').to_number(), 64007000005500301)
-		self.assertEqual(String('만오천').to_number(), 15000)
-		self.assertEqual(String('일').to_number(), 1)
-		self.assertEqual(String('영').to_number(), 0)
+    def test_to_number(self):
+        self.assertEqual(String('천이백십일억천백만').to_number(), 121111000000)
+        self.assertEqual(String('삼천이백십일억천백만오십칠').to_number(), 321111000057)
+        self.assertEqual(String('육경사천칠조오백오십만삼백일').to_number(), 64007000005500301)
+        self.assertEqual(String('만오천').to_number(), 15000)
+        self.assertEqual(String('일').to_number(), 1)
+        self.assertEqual(String('영').to_number(), 0)
 
-	def test_isnumeric(self):
-		self.assertEqual(String('영').isnumeric(), True)
-		self.assertEqual(String('천이백십일억천백만').isnumeric(), True)
-		self.assertEqual(String('천이백십일억천백만').isnumeric(), True)
-		self.assertEqual(String('one').isnumeric(), False)
+    def test_isnumeric(self):
+        self.assertEqual(String('영').isnumeric(), True)
+        self.assertEqual(String('천이백십일억천백만').isnumeric(), True)
+        self.assertEqual(String('천이백십일억천백만').isnumeric(), True)
+        self.assertEqual(String('one').isnumeric(), False)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
